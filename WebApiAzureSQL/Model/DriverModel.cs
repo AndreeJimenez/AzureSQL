888e408b76bb2df861897a52fff8176632106ad7
Andree Jimenez <andree_jimenez@outlook.com>
2020-06-04 02:26:23 -0500

    Create WebApiAzureSQL

---
+++ WebApiAzureSQL/Model/DriverModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiAzureSQL.Model
+{
+    public class DriverModel
+    {
+        string ConnectionString = "Server=tcp:azuredatabasesqlserver.database.windows.net,1433;Initial Catalog=AzureSqlDatabase;Persist Security Info=False;User ID={ your userID };Password={ your password };MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public int IDDriver { get; set; }
+
+        public string Name { get; set; }
+
+        public string Picture { get; set; }
+
+        public string Status { get; set; }
+
+        public PositionModel ActualPosition { get; set; }
+
+        public List<DriverModel> GetAll()
+        {
+            List<DriverModel> list = new List<DriverModel>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+                    string tsql = "SELECT * FROM Driver INNER JOIN Position ON Driver.IDActualPosition = Position.IDPosition";
+                    using (SqlCommand cmd = new SqlCommand(tsql, conn))
+                    {
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                list.Add(new DriverModel
+                                {
+                                    IDDriver = (int)reader["IDDriver"],
+                                    Name = reader["Name"].ToString(),
+                                    Status = reader["Status"].ToString(),
+                                    Picture = reader["Picture"].ToString(),
+                                    ActualPosition = new PositionModel
+                                    {
+                                        Latitude = reader["Latitude"].ToString(),
+                                        Longitude = reader["Longitude"].ToString()
+                                    }
+                                });
+                            }
+                        }
+                    }
+                }
+                return list;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ WebApiAzureSQL/Model/PositionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiAzureSQL.Model
+{
+    public class PositionModel
+    {
+        public string Latitude { get; set; }
+
+        public string Longitude { get; set; }
+    }
+}
